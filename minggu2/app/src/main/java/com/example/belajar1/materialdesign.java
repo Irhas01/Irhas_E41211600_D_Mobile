c6433c37e041628de3b3b0b1c806f21146d6e601
Irhas01 <113319982+Irhas01@users.noreply.github.com>
2023-02-21 18:34:28 +0700

    pro

---
+++ minggu2/app/src/main/java/com/example/belajar1/materialdesign.java
@@ -0,0 +1,14 @@
+package com.example.belajar1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class materialdesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.materialdesign);
+    }
+}
